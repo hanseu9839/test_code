1ca4c77b1b17358d7ffe3e913bac45bda60ca207
hanseounggyun <hanseu9839@gmail.com>
2024-07-22 01:44:17 +0900

    UserControllerTestCode

---
+++ src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -0,0 +1,126 @@
+package com.example.demo.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import com.example.demo.model.UserStatus;
+import com.example.demo.model.dto.UserUpdateDto;
+import com.example.demo.repository.UserEntity;
+import com.example.demo.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@SqlGroup({
+        @Sql(value = "/sql/user-controller-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void 사용자는_특정_유저_정보를_소거된_채_갖고_올_수_있다() throws Exception {
+        // given
+        // when
+        // then
+        mockMvc.perform(get("/api/users/11")
+                        .header("EMAIL", "hanseu9839@gmail.com"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(11))
+                .andExpect(jsonPath("$.email").value("hanseu9839@gmail.com"))
+                .andExpect(jsonPath("$.nickname").value("hanseu9839"))
+                .andExpect(jsonPath("$.address").doesNotExist())
+                .andExpect(jsonPath("$.status").value("ACTIVE"));
+    }
+
+
+    @Test
+    void 사용자는_특정_유저의_정보를_전달_받을_수_있다() throws Exception {
+        // given
+        // when
+        // then
+        mockMvc.perform(get("/api/users/11"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(11))
+                .andExpect(jsonPath("$.email").value("hanseu9839@gmail.com"))
+                .andExpect(jsonPath("$.nickname").value("hanseu9839"))
+                .andExpect(jsonPath("$.status").value("ACTIVE"));
+    }
+
+    @Test
+    void 사용자는_존재하지_않는_유저의_아이디로_api_호출할_경우_404_응답을_받는다() throws Exception {
+        // given
+        // when
+        // then
+        mockMvc.perform(get("/api/users/1134"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("Users에서 ID 1134를 찾을 수 없습니다."));
+    }
+
+    @Test
+    void 사용자는_인증_코드로_계정을_활성화_시킬_수_있다() throws Exception {
+        // given
+        // when
+        // then
+        mockMvc.perform(get("/api/users/12/verify").queryParam("certificationCode","aaaaaaaaaaaa-aaaaaaa-aaaaaaaaaab"));
+        UserEntity userEntity = userRepository.findById(12L).get();
+        assertThat(userEntity.getStatus()).isEqualTo(UserStatus.ACTIVE);
+    }
+
+    @Test
+    void 사용자는_내_정보를_불러올_떄_개인정보인_주소도_갖고_올_수_있다() throws Exception {
+        // given
+        // when
+        // then
+        mockMvc.perform(get("/api/users/me")
+                .header("EMAIL", "hanseu9839@gmail.com"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(11))
+                .andExpect(jsonPath("$.email").value("hanseu9839@gmail.com"))
+                .andExpect(jsonPath("$.nickname").value("hanseu9839"))
+                .andExpect(jsonPath("$.address").value("Seoul"))
+                .andExpect(jsonPath("$.status").value("ACTIVE"));
+    }
+
+    @Test
+    void 사용자는_내_정보를_수정할_수_있다() throws Exception {
+        // given
+        UserUpdateDto userUpdateDto = UserUpdateDto.builder()
+                .nickname("hanseu9839-h")
+                .address("cheonan")
+                .build();
+
+        // when
+        // then
+        mockMvc.perform(put("/api/users/me")
+                        .header("EMAIL", "hanseu9839@gmail.com")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userUpdateDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(11))
+                .andExpect(jsonPath("$.email").value("hanseu9839@gmail.com"))
+                .andExpect(jsonPath("$.nickname").value("hanseu9839-h"))
+                .andExpect(jsonPath("$.address").value("cheonan"))
+                .andExpect(jsonPath("$.status").value("ACTIVE"));
+    }
+}
