9c56c34240f49725a2d354267a5ec204fb3aa1d9
hanseounggyun <hanseu9839@gmail.com>
2024-07-22 02:17:53 +0900

    게시물을 테스트코드 작성 완료

---
+++ src/test/resources/sql/post-controller-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id, email, nickname, address, certification_code, status, last_login_at) VALUES (11, 'hanseu9839@gmail.com', 'hanseu9839', 'Seoul', 'aaaaaaaaaaaa-aaaaaaa-aaaaaaaaaa', 'ACTIVE', 0);
+
+INSERT INTO posts (id, content, created_at, modified_at, user_id) values(11, 'helloworld', 1678530673958, 0, 11)
---
+++ src/test/resources/sql/post-create-controller-data.sql
@@ -0,0 +1,1 @@
+INSERT INTO users (id, email, nickname, address, certification_code, status, last_login_at) VALUES (11, 'hanseu9839@gmail.com', 'hanseu9839', 'Seoul', 'aaaaaaaaaaaa-aaaaaaa-aaaaaaaaaa', 'ACTIVE', 0);
